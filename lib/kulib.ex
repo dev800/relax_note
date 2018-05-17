e1d4297a37738d80ffca40bf9795669d12eddb72
WangJinzhong <andywang7259@163.com>
2018-05-17 12:15:16 +0800

    init Project

---
+++ lib/kulib.ex
@@ -0,0 +1,18 @@
+defmodule Kulib do
+  @moduledoc """
+  Documentation for Kulib.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kulib.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
